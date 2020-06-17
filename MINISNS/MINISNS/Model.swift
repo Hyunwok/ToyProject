e0cb9087cce32964a6b5bdf470d3ddb9bd57059e
이현욱 <hjhsha827@gmail.com>
2020-06-17 22:49:28 +0900

    [CREATE]Model 생성

---
+++ MINISNS/MINISNS/Model.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+struct LogInModel: Codable {
+    let id: String
+    let pw: String
+}
+
+struct RegisterModel: Codable {
+    let id: String
+    let pw: String
+    let num: String
+    let name: String
+}
+
+struct FindIdModel: Codable {
+    let name: String
+    let email: String?
+    let num: String?
+}
--- MINISNS/MINISNS/Network.swift
+++ MINISNS/MINISNS/Network.swift
@@ -2,7 +2,16 @@ import UIKit
 import Alamofire
 
 class Network {
+
+    fileprivate var baseUrl = ""
     
+    init(baseUrl: String) {
+        self.baseUrl = baseUrl
+    }
+    
+    func post(endPoint: String) {
+        AF.request(baseUrl + endPoint, method: .post).validate()
+    }
    
 }
 
