beb2760b542f9b66d9202fffbb68856dc2e2ba09
이현욱 <hjhsha827@gmail.com>
2020-06-22 22:19:44 +0900

    [FIX]RegisterVC 로직 완성
    거의 완성

--- MINISNS/MINISNS/VC/RegisterVC.swift
+++ MINISNS/MINISNS/VC/RegisterVC.swift
@@ -1,11 +1,10 @@
 import UIKit
 import Alamofire
 
+
 // MARK: RegisterVC
 
 class RegisterVC: UIViewController {
-    
-    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var phoneNumberTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -20,46 +19,34 @@ class RegisterVC: UIViewController {
     
     @IBAction func getRegister(_ sender: UIButton) {
         
-        let url = "https://jsonplaceholder.typicode.com/todosx"
-//
-//        "email":emailTextField.text!,
-//          "phone":phoneNumberTextField.text!,
-//          "password":pwTextField.text!
+        let url = "http://13.209.77.9:3000/api/auth/register"
         
-        let param : Parameters = [
-                                    "userId":nameTextField.text!,
-                                    "id":emailTextField.text!,
-                                    "title":pwTextField.text!,
-                                    "completed":true
-            
-        ]
+        if nameTextField.text!.isEmpty ||
+            phoneNumberTextField.text!.isEmpty ||
+            emailTextField.text!.isEmpty ||
+            pwTextField.text!.isEmpty {
+            self.presentAlert(title: "에러", message: "채우지 않은 공간이 있습니다.")
+            return
+        }
         
-//        if nameTextField.text!.isEmpty ||
-//            phoneNumberTextField.text!.isEmpty ||
-//            emailTextField.text!.isEmpty ||
-//            pwTextField.text!.isEmpty {
-//            self.presentAlert(title: "에러", message: "채우지 않은 공간이 있습니다.")
-//            return
-//        }
+         let param : Parameters = [
+             "username":nameTextField.text!,
+             "password":pwTextField.text!,
+             "email":emailTextField.text!,
+             "phone":phoneNumberTextField.text!
+         ]
         
-        AF.request(url, method: .post, parameters: param, encoding: JSONEncoding.default).responseJSON { (response) in 
-            if let value = response.value as? [String:Any] {
-                print(value)
-            } else {
-                print("실패")
-            }
+        AF.request(url, method: .post, parameters: param, encoding: JSONEncoding.default).responseJSON {
+            (response) in
             if let status = response.response?.statusCode {
                 switch status {
-                case 201:
+                case 200:
                     self.presentAlert(title: "회원가입 성공", message: "회원가입에 성공하셨습니다.")
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                         self.navigationController?.popViewController(animated: true)
                     }
-                case 204: self.presentAlert(title: "회원가입 실패", message: "아이디가 있거나 형식에 맞지 않습니다")
-                case 205: self.presentAlert(title: "회원가입 실패", message: "아이디가 있거나 형식에 맞지 않습니다")
-                default:
-                    print(1)
-                    return
+                case 409: self.presentAlert(title: "회원가입 실패", message: "아이디가 있거나 형식에 맞지 않습니다")
+                default: return
                 }
             }
         }
