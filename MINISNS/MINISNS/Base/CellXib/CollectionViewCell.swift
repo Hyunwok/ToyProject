79c0031f11756617ab30a6b6a798578035b5899d
이현욱 <hjhsha827@gmail.com>
2020-06-24 08:42:42 +0900

    [CREATE] CollectionViewCell, Xib 생성

---
+++ MINISNS/MINISNS/Base/CellXib/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  MINISNS
+//
+//  Created by 이현욱 on 2020/06/24.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
