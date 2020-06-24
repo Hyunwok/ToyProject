dd8ce477d7e6131d85f038d1c220603e49267fb9
이현욱 <hjhsha827@gmail.com>
2020-06-24 20:58:32 +0900

    [CREATE] WriteVC 추가

---
+++ MINISNS/MINISNS/VC/WriteVC.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// MARK: WriteVC
+
+class WriteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+//    actionSheet.addAction(UIAlertAction(title: "카메라", style: .default, handler: { (_) in
+//        self.imagePicker.sourceType = .camera
+//        self.present(self.imagePicker, animated: true, completion: nil)
+//    }))
+//    
+//    actionSheet.addAction(UIAlertAction(title: "사진첩", style: .default, handler: { (_) in
+//        self.imagePicker.sourceType = .photoLibrary
+//        self.present(self.imagePicker, animated: true, completion: nil)
+//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//           
+//                print(info)
+//            
+//            self.dismiss(animated: true, completion: nil)
+//        }
+//    }))
+}
