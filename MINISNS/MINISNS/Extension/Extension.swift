42734463dda71bbed81b141f135a3fa84be64278
이현욱 <hjhsha827@gmail.com>
2020-06-27 21:53:37 +0900

    [CREATE] Extension 파일 생성

---
+++ MINISNS/MINISNS/Extension/Extension.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  MINISNS
+//
+//  Created by 이현욱 on 2020/06/27.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import UIKit
+
+extension WriteVC {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func keyboardWillShow(_ sender: Notification) {
+        self.view.frame.origin.y = -150
+    }
+    
+    @objc func keyboardWillDisa(_ sender: Notification) {
+        self.view.frame.origin.y = 0
+    }
+}
