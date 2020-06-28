0792652eb0bb86a3c9867ef674d8ddd40976eed4
이현욱 <hjhsha827@gmail.com>
2020-06-28 21:39:03 +0900

    [CREATE] ToyUI 프로젝트 생성

---
+++ ToyUI/ToyUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToyUI
+//
+//  Created by 이현욱 on 2020/06/28.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
