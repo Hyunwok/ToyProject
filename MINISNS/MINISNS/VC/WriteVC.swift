6ecb26683997f71582cda0a31b9e6bef36dec575
이현욱 <hjhsha827@gmail.com>
2020-07-01 23:04:11 +0900

    [FIX] 코드 여전히 수정 중

--- MINISNS/MINISNS/VC/WriteVC.swift
+++ MINISNS/MINISNS/VC/WriteVC.swift
@@ -5,9 +5,14 @@ import Alamofire
 
 class WriteVC: UIViewController {
     
-    var imageData: Data!
-    var url:NSURL!
+    let imagePicker = UIImagePickerController()
+
+    var image: Data!
+    var content: String!
+    //var url: URL!
+    var asd: String = ""
     
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var imageViewNilLbl: UILabel!
     @IBOutlet weak var textView: UITextView!
@@ -16,6 +21,8 @@ class WriteVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        loadingIndicator.isHidden = true
+        imagePicker.delegate = self
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisa(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
@@ -25,49 +32,39 @@ class WriteVC: UIViewController {
     }
     
     @IBAction func addRead(_ sender: Any) {
-        let param: [String:String] = ["file":"\(imageData!)","content":textView.text!]
-        let header: HTTPHeaders = ["Content-Type":"multipart/form-data","access_token":"\(UserDefaults.standard.string(forKey: "token")!)"]
-        if imageView.image == nil || ((titleTextField.text?.isEmpty) == true) {
-            let alert = UIAlertController(title: "실패", message: "사진과 글이 준비되지 않았습니다.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
-            present(alert, animated: true, completion: nil)
+        if imageView.image == nil || ((titleTextField.text?.isEmpty)! == true) || titleTextField.text == "" {
+            self.presentAlert(title: "실패", message: "사진과 글이 준비되지 않았습니다."); return
         }
 
-//        AF.upload(multipartFormData: { (MultipartFormData) in
-//            MultipartFormData.append(self.imageData, withName: self.titleTextField.text!, fileName: "sample", mimeType: "image/jpeg")
-//            for (key, value) in param {
-//                MultipartFormData.append((value.data(using: .utf8))!, withName: key)
-//            }}, to: "http://10.156.145.141:3000/post", method: .post, headers: header).responseJSON { (result) in
-//                debugPrint(result)
-//                switch result.response?.statusCode {
-//                case 200: print(1)
-//                case 403: print(2)
-//                case 500: print(3)
-//                default: print(4)
-//                }
-//                debugPrint(result)
+        self.getLoadingIndicator(value: false)
+        let header: HTTPHeaders = ["Content-Type":"application/json",
+                                   "access_token":"\(UserDefaults.standard.string(forKey: "token")!)"]
+         AF.upload(multipartFormData: { (MultipartFormData) in
+            MultipartFormData.append(self.image!, withName: "img", fileName: "img", mimeType: "img/jpeg")}, to: "http://10.156.145.141:3000/post", method: .post, headers: header).responseJSON { (result) in
+                        debugPrint(result)
+        }
+//
+//        AF.request("http://10.156.145.141:3000/post", method: .post, parameters: param, encoding: JSONEncoding.default, headers: header).responseJSON { response in
+//            debugPrint(response)
+//            switch response.response?.statusCode {
+//            case 200:
+//                let alert = UIAlertController(title: "성공", message: "글이 올라갔습니다", preferredStyle: .alert)
+//                alert.addAction(UIAlertAction(title: "확인", style: .default, handler: {(_) in
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                        self.presentVC(identifier: "MainVC")}
+//                    self.getLoadingIndicator(value: true)
+//                    MainVC.data.append("")
+//                }))
+//                self.present(alert, animated: true, completion: nil)
+//            case 403:
+//                self.presentAlert(title: "실패", message: "로그인이 되어있지 않습니다.")
+//                self.getLoadingIndicator(value: true)
+//            case 500:
+//                self.presentAlert(title: "에러", message: "작성 실패")
+//                self.getLoadingIndicator(value: true)
+//            default: self.getLoadingIndicator(value: true); return
+//            }
 //        }
-
-
-        
-        AF.request("http://10.156.145.141:3000/post", method: .post, parameters: param, encoding: JSONEncoding.default, headers: header, interceptor: nil, requestModifier: nil).responseJSON { response in
-            debugPrint(response)
-            switch response.response?.statusCode {
-            case 200:
-                let alert = UIAlertController(title: "성공", message: "글이 올라갔습니다", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "확인", style: .default, handler: {(_) in
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        self.presentVC(identifier: "MainVC")}
-                    MainVC.data.append("")
-                }))
-                self.present(alert, animated: true, completion: nil)
-            case 403:
-                self.presentAlert(title: "실패", message: "로그인이 되어있지 않습니다.")
-            case 500:
-                self.presentAlert(title: "에러", message: "작성 실패")
-            default: return
-            }
-        }
     }
     
     @IBAction func goToMainBtn(_ sender: UIButton) {
@@ -87,6 +84,17 @@ class WriteVC: UIViewController {
 // MARK: Extension
 
 extension WriteVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    func getLoadingIndicator(value: Bool) {
+        switch value {
+        case true:
+            self.loadingIndicator.stopAnimating()
+            self.loadingIndicator.isHidden = value
+        case false:
+            self.loadingIndicator.startAnimating()
+            self.loadingIndicator.isHidden = value
+        }
+    }
+    
     func presentVC(identifier: String) {
         guard let vc = storyboard?.instantiateViewController(identifier: identifier) else { return }
         vc.modalPresentationStyle = .fullScreen
@@ -100,17 +108,15 @@ extension WriteVC: UIImagePickerControllerDelegate, UINavigationControllerDelega
     }
     
     func tapImageView() {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
         let actionSheet = UIAlertController(title: "선택", message: nil, preferredStyle: .actionSheet)
         actionSheet.addAction(UIAlertAction(title: "카메라", style: .default, handler: { (_) in
-            imagePicker.sourceType = .camera
-            self.present(imagePicker, animated: true, completion: nil)
+            self.imagePicker.sourceType = .camera
+            self.present(self.imagePicker, animated: true, completion: nil)
         }))
         actionSheet.addAction(UIAlertAction(title: "사진첩", style: .default, handler: { (_) in
-            imagePicker.sourceType = .photoLibrary
-            imagePicker.allowsEditing = true
-            self.present(imagePicker, animated: true, completion: nil)
+            self.imagePicker.sourceType = .photoLibrary
+            self.imagePicker.allowsEditing = true
+            self.present(self.imagePicker, animated: true, completion: nil)
         }))
         actionSheet.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
         self.present(actionSheet, animated: true, completion: nil)
@@ -118,12 +124,10 @@ extension WriteVC: UIImagePickerControllerDelegate, UINavigationControllerDelega
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let pickedImage = info[.originalImage] as? UIImage else { return }
-            
-        //data.write
+        image = pickedImage.jpegData(compressionQuality: 0.1)!
         imageView.image = pickedImage
-
-        imageData = pickedImage.jpegData(compressionQuality: 1)
         imageViewNilLbl.isHidden = true
+        //url = info[.phAsset] as? URL
         picker.dismiss(animated: true, completion: nil)
     }
     
