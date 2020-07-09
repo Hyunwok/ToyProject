568ae989c7d9838fe8aca948aad241702c9adcc9
이현욱 <hjhsha827@gmail.com>
2020-07-10 08:44:49 +0900

    [Create] MemoAlarm 프로젝트 생성

---
+++ MemoAlarm/MemoAlarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoAlarm
+//
+//  Created by 이현욱 on 2020/07/10.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
