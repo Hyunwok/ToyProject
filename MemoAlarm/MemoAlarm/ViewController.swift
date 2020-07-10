a5a22d425f60a14f1dbccfb6a42901a05be87a51
이현욱 <hjhsha827@gmail.com>
2020-07-10 11:59:35 +0900

    [Add] 코드 추가

--- MemoAlarm/MemoAlarm/ViewController.swift
+++ MemoAlarm/MemoAlarm/ViewController.swift
@@ -1,20 +1,41 @@
-//
-//  ViewController.swift
-//  MemoAlarm
-//
-//  Created by 이현욱 on 2020/07/10.
-//  Copyright © 2020 이현욱. All rights reserved.
-//
-
 import UIKit
+import FSCalendar
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FSCalendarDelegate, FSCalendarDataSource {
+    
+    var list = [String]()
 
+    @IBOutlet weak var alarmPlusBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var calendarView: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calendarView.appearance.titleWeekendColor = .red
+        calendarView.appearance.headerMinimumDissolvedAlpha = 0.0;
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    @IBAction func piusAlarm(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        return cell
+    }
+    
+    
+
+}
