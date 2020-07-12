805e312b9bfb65518681e0e8d6720ba528bc9fa1
이현욱 <hjhsha827@gmail.com>
2020-07-12 22:55:23 +0900

    [Fix] DatePickerXib 수정

--- MemoAlarm/MemoAlarm/Base/Xib/DatePickerXib.swift
+++ MemoAlarm/MemoAlarm/Base/Xib/DatePickerXib.swift
@@ -1,27 +1,26 @@
 import UIKit
 
-class DatePickerXib: UIView {
+final class DatePickerXib: UIView {
     
     private let xibName = "DatePickerView"
     var alarmText: String!
-
+    
+    @IBOutlet weak var alarmTextField: UITextField!
     @IBOutlet weak var lbl: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var okBtn: UIButton!
-        
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.commonInit()
-        datePicker.addTarget(self, action: #selector(abc), for: .valueChanged)
+        datePicker.addTarget(self, action: #selector(getTime), for: .valueChanged)
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        self.commonInit()
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
     
     private func commonInit(){
-        guard let xibName = NSStringFromClass(self.classForCoder).components(separatedBy: ".").last else { return }
         let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as! UIView
         view.frame = self.bounds
         self.addSubview(view)
@@ -31,7 +30,7 @@ class DatePickerXib: UIView {
         
     }
     
-    @objc func abc() {
+    @objc func getTime() {
         let formatter = DateFormatter()
         formatter.dateStyle = .short
         let date = formatter.string(from: datePicker.date)
