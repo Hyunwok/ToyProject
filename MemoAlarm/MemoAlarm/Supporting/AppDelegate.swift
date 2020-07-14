9d701655358d6381ac1ff28a7a61dea622d5dc0b
이현욱 <hjhsha827@gmail.com>
2020-07-14 23:04:48 +0900

    [Fix] 코드 및 UI 수정

--- MemoAlarm/MemoAlarm/Supporting/AppDelegate.swift
+++ MemoAlarm/MemoAlarm/Supporting/AppDelegate.swift
@@ -25,8 +25,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+       
+    }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+//        if(application.applicationState == .active) {
+//
+//        }
+//        if(application.applicationState == .inactive) {
+//
+//        }
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
