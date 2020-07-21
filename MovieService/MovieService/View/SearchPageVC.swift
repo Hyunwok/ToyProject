490a8b6cca143a2eb2c54b9e52d48963cefb8d7f
이현욱 <hjhsha827@gmail.com>
2020-07-21 23:01:22 +0900

    [Add] SearchPageVC 생성 및 코드 추가

---
+++ MovieService/MovieService/View/SearchPageVC.swift
@@ -0,0 +1,33 @@
+//
+//  SearchVC.swift
+//  MovieService
+//
+//  Created by 이현욱 on 2020/07/21.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import UIKit
+
+class SearchPageVC: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
