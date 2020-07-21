df1a358523b1e0e86bfeb2e50b05b2ce83802e66
이현욱 <hjhsha827@gmail.com>
2020-07-21 14:59:50 +0900

    [Add] ViewController 코드 추가

---
+++ MovieService/MovieService/View/ViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+    let viewModel = MovieViewModel()
+    //키 : key=c16f104dccf7480daa4204c8f921d8b6
+    //예시 : http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=c16f104dccf7480daa4204c8f921d8b6..&targetDt=20200720
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.loadView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.loadView()
+    }
+}
+
