5e3fdaae892958772d65e5fb4900991d3a8dd056
이현욱 <hjhsha827@gmail.com>
2020-07-21 23:01:37 +0900

    [Add] RegisterVC 생성 및 추가

---
+++ MovieService/MovieService/View/RegisterVC.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterVC.swift
+//  MovieService
+//
+//  Created by 이현욱 on 2020/07/21.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
