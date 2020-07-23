152288ad92e8fc3fee0699583b950c3faa576d42
이현욱 <hjhsha827@gmail.com>
2020-07-23 20:29:41 +0900

    [Add] Model 파일 추가

---
+++ MemoAlarm/MemoAlarm/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  MemoAlarm
+//
+//  Created by 이현욱 on 2020/07/23.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import Foundation
+
+struct List {
+    let dateList: String
+    let dateListText: String
+}
