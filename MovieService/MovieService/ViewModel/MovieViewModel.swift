1dfccc7082941e43609d1fc6ecbc334cef4eb079
이현욱 <hjhsha827@gmail.com>
2020-07-25 23:53:30 +0900

    [Add] MovieViewModel 코드 추가

--- MovieService/MovieService/ViewModel/MovieViewModel.swift
+++ MovieService/MovieService/ViewModel/MovieViewModel.swift
@@ -1,11 +1,22 @@
 import Foundation
 import RxSwift
 import RxCocoa
-import RxAlamofire
+import Alamofire
 
 class MovieViewModel {
-    func loadView() {
-        
+    func loadView() -> Observable<Any> {
+        let key = "c16f104dccf7480daa4204c8f921d8b6"
+        let url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(Date())"
+        return Observable<Any>.create( { observer in
+            AF.request(url, method: .get).responseJSON { response in
+                switch response.response?.statusCode {
+                case 200: return observer.onNext(1)
+                case 400, 500: return observer.onError(response.error as! Error)
+                default: return observer.onError(response.error as! Error)
+                }
+                observer.onCompleted()
+                } as! Disposable
+        })
     }
 }
 
