fa8a2b581d7c080ff2d94dace8b119c9b6f846ec
이현욱 <hjhsha827@gmail.com>
2020-07-27 23:39:01 +0900

    [Add] Extension 코드 추가

--- MovieService/MovieService/Extension/Extension.swift
+++ MovieService/MovieService/Extension/Extension.swift
@@ -1,6 +1,24 @@
 import UIKit
 
 extension UIViewController {
+
+    func presentAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func presentVC(identifier: String) {
+        guard let vc = self.storyboard?.instantiateViewController(identifier: identifier) else { return }
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    func pushVC(identifier: String) {
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: identifier) else { return }
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
     open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         self.view.endEditing(true)
     }
