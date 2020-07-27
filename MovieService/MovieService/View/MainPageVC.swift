af11d11988f39d25703cda045cdf21fa853a7008
이현욱 <hjhsha827@gmail.com>
2020-07-27 23:39:34 +0900

    [Fix] MainPageVC 코드 수정

--- MovieService/MovieService/View/MainPageVC.swift
+++ MovieService/MovieService/View/MainPageVC.swift
@@ -6,7 +6,7 @@ import Alamofire
 class MainPageVC: UIViewController {
     
     let viewModel = MovieViewModel()
-    let movieArray = [MovieInfo]()
+    let movieArray = [MovieInfoIn]()
     var movieName: String!
     let disposeBag = DisposeBag()
     
@@ -14,7 +14,7 @@ class MainPageVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel.loadView("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=430156241533f1d058c603178cc3ca0e")
+        viewModel.loadView(url: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=430156241533f1d058c603178cc3ca0e")
         AF.request("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=430156241533f1d058c603178cc3ca0e", method: .get).responseJSON { response in
             switch response.response?.statusCode {
             case 200: guard let value = response.value as? [String:Any] else { return }
@@ -24,13 +24,13 @@ class MainPageVC: UIViewController {
             guard let movieRank = movieInfo["rank"] as? String else { return }
             guard let openDate = movieInfo["openDt"] as? String else { return }
             guard let audi = movieInfo["audiAcc"] as? String else { return }
-            MovieInfo.init(rank: movieRank, movieName: movieName, movieOpenDate: openDate, cumulativeAudience: audi)
+            MovieInfoIn.init(rank: movieRank, movieNm: movieName, openDt: openDate, audiAcc: audi)
             case 404: return
             default: return
             }
         }
         
-        let observable: Observable<[MovieInfo]> = Observable.just(movieArray)
+        let observable: Observable<[MovieInfoIn]> = Observable.just(movieArray)
         observable.bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { _, movie, cell in
             if let cellToUse = cell as? MovieTableCell {
              //   cellToUse.movieName.text = MovieInfo().ran
@@ -44,10 +44,5 @@ class MainPageVC: UIViewController {
     }
 }
 
-struct MovieInfo {
-    let rank: String
-    let movieName: String
-    let movieOpenDate: String
-    let cumulativeAudience: String
-}
+
 
