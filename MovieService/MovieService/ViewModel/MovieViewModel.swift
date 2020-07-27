80cb4bda0697655fa49723d8a40002301712f95a
이현욱 <hjhsha827@gmail.com>
2020-07-27 23:40:09 +0900

    [Fix] MovieViewModel 코드 수정

--- MovieService/MovieService/ViewModel/MovieViewModel.swift
+++ MovieService/MovieService/ViewModel/MovieViewModel.swift
@@ -3,20 +3,36 @@ import RxSwift
 import RxCocoa
 import Alamofire
 
+struct MovieInfoOut: Codable {
+    let dailyBoxOfficeList: MovieInfoIn
+}
+
+struct MovieInfoIn: Codable {
+    let rank: String
+    let movieNm: String
+    let openDt: String
+    let audiAcc: String
+}
+
 class MovieViewModel {
+    
     private let key = "c16f104dccf7480daa4204c8f921d8b6"
     
     func loadView(url: String) -> Observable<Any> {
-        let url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(key)&targetDt=\(Date())"
+        let url =
+        "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/dailyBoxOfficeList/searchDailyBoxOfficeList.json?key=\(key)&targetDt=20200726"
         return Observable<Any>.create( { observer in
             AF.request(url, method: .get).responseJSON { response in
-                switch response.response?.statusCode {
-                case 200: return observer.onNext(1)
-                case 400, 500: return observer.onError(response.error as! Error)
-                default: return observer.onError(response.error as! Error)
-                }
-                observer.onCompleted()
-                } as! Disposable
+                switch response.result {
+                case .success(let json):
+                    do {
+                        let model = try JSONDecoder().decode(MovieInfoOut.self, from: json as! Data)
+                        print(model)
+                        observer.onNext(model)
+                    } catch let error {
+                        observer.onError(error)
+                    }
+                case .failure(let error): observer.onError(error) } } as! Disposable
         })
     }
 }
@@ -24,6 +40,4 @@ class MovieViewModel {
 class LogInViewModel {
     let idObservable = PublishSubject<String>()
     let pwObservable = PublishSubject<String>()
-    
-    
 }
