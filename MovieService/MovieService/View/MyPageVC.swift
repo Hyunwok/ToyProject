ce5e32425f7a718ea318bbcbfefbad57b9661a2f
이현욱 <hjhsha827@gmail.com>
2020-07-28 23:23:39 +0900

    [Add] MyPageVC 코드 추가

--- MovieService/MovieService/View/MyPageVC.swift
+++ MovieService/MovieService/View/MyPageVC.swift
@@ -3,7 +3,13 @@ import Firebase
 
 class MyPageVC: UIViewController {
 
+    @IBOutlet weak var logOutBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func getLogOut(_ sender: Any) {
+        self.presentVC(identifier: "LoginVC")
     }
 }
