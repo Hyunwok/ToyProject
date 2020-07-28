754563de08fbba7cb8b80ff4738b7da9fda7263a
이현욱 <hjhsha827@gmail.com>
2020-07-28 16:34:59 +0900

    [Fix] MainPageVC 코드 수정

--- MovieService/MovieService/View/MainPageVC.swift
+++ MovieService/MovieService/View/MainPageVC.swift
@@ -5,42 +5,24 @@ import Alamofire
 
 class MainPageVC: UIViewController {
     
+    private let key = "430156241533f1d058c603178cc3ca0e"
     let viewModel = MovieViewModel()
-    let movieArray = [MovieInfoIn]()
+    //    let movieArray = [MovieInfoOut]()
     var movieName: String!
     let disposeBag = DisposeBag()
+    
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel.loadView(url: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=430156241533f1d058c603178cc3ca0e")
-        AF.request("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=430156241533f1d058c603178cc3ca0e", method: .get).responseJSON { response in
-            switch response.response?.statusCode {
-            case 200: guard let value = response.value as? [String:Any] else { return }
-            let searchRes = value["movieListResult"] as? [String:Any]
-            guard let movieInfo = searchRes?["movieList"] as? [String:Any] else { return }
-            guard let movieName = movieInfo["movieNm"] as? String else { return }
-            guard let movieRank = movieInfo["rank"] as? String else { return }
-            guard let openDate = movieInfo["openDt"] as? String else { return }
-            guard let audi = movieInfo["audiAcc"] as? String else { return }
-            MovieInfoIn.init(rank: movieRank, movieNm: movieName, openDt: openDate, audiAcc: audi)
-            case 404: return
-            default: return
-            }
-        }
         
-        let observable: Observable<[MovieInfoIn]> = Observable.just(movieArray)
+        let observable = viewModel.loadView(url: "boxoffice/searchDailyBoxOfficeList.json?key=\(self.key)&targetDt=20120101").bind(to)
         observable.bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { _, movie, cell in
             if let cellToUse = cell as? MovieTableCell {
-             //   cellToUse.movieName.text = MovieInfo().ran
+                cellToUse.movieName.text = MovieInfo().ran
             }
         }.disposed(by: disposeBag)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        viewModel.loadView(url: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=430156241533f1d058c603178cc3ca0e")
     }
 }
 
