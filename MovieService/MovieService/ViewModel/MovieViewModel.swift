dc1c9f470e234df45a2e6faa5c21330d8e699e42
이현욱 <hjhsha827@gmail.com>
2020-07-28 23:24:35 +0900

    [Fix] MovieViewModel 코드 수정

--- MovieService/MovieService/ViewModel/MovieViewModel.swift
+++ MovieService/MovieService/ViewModel/MovieViewModel.swift
@@ -1,43 +1,19 @@
 import Foundation
-import RxSwift
-import RxCocoa
-import Alamofire
 
-struct MovieInfoOut: Codable {
-    let dailyBoxOfficeList: MovieInfoIn
+struct Result: Decodable {
+    let boxOfficeResult: ResultList
 }
 
-struct MovieInfoIn: Codable {
-    let rank: String
-    let movieNm: String
+struct ResultList: Decodable {
+    let dailyBoxOfficeList: [List]
+}
+
+struct List: Decodable {
     let openDt: String
     let audiAcc: String
+    let rank: String
+    let movieNm: String
 }
 
-class MovieViewModel {
-    
-    private let key = "c16f104dccf7480daa4204c8f921d8b6"
-    
-    func loadView(url: String) -> Observable<Any> {
-        let url =
-        "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/dailyBoxOfficeList/searchDailyBoxOfficeList.json?key=\(key)&targetDt=20200726"
-        return Observable<Any>.create( { observer in
-            AF.request(url, method: .get).responseJSON { response in
-                switch response.result {
-                case .success(let json):
-                    do {
-                        let model = try JSONDecoder().decode(MovieInfoOut.self, from: json as! Data)
-                        print(model)
-                        observer.onNext(model)
-                    } catch let error {
-                        observer.onError(error)
-                    }
-                case .failure(let error): observer.onError(error) } } as! Disposable
-        })
-    }
-}
 
-class LogInViewModel {
-    let idObservable = PublishSubject<String>()
-    let pwObservable = PublishSubject<String>()
-}
+
