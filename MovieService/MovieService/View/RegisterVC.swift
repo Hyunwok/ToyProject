9b510a2d6f77016d82d66743570d30cfeaa8ba89
이현욱 <hjhsha827@gmail.com>
2020-07-29 22:42:53 +0900

    [Fix] 코드 수정

--- MovieService/MovieService/View/RegisterVC.swift
+++ MovieService/MovieService/View/RegisterVC.swift
@@ -5,6 +5,7 @@ class RegisterVC: UIViewController {
     
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var getBackToLoginVC: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +22,10 @@ class RegisterVC: UIViewController {
                                     self.presentAlert(title: "회원가입 실패", message: "에러가 있습니다.")
                                     return
                                 }
-                                self.presentVC(identifier: "LoginVC")
+                                self.presentAlert(title: "회원가입 성공", message: "축하합니다!")
+                                DispatchQueue.global().async {
+                                    self.presentVC(identifier: "LoginVC")
+                                }
         }
     }
 }
