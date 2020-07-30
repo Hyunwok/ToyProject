a85fd0148e34bdaa3f0483da39bf8ec542207806
이현욱 <hjhsha827@gmail.com>
2020-07-30 23:51:53 +0900

    [Fix] MainPageVC 코드 수정

--- MovieService/MovieService/View/MainPageVC.swift
+++ MovieService/MovieService/View/MainPageVC.swift
@@ -6,23 +6,22 @@ import Alamofire
 class MainPageVC: UIViewController {
     
     private let key = "430156241533f1d058c603178cc3ca0e"
-    let viewModel = MovieViewModel()
-    //    let movieArray = [MovieInfoOut]()
+    let viewModel = MovieReloadViewModel()
+    
     var movieName: String!
     let disposeBag = DisposeBag()
-    
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let observable = viewModel.loadView(url: "boxoffice/searchDailyBoxOfficeList.json?key=\(self.key)&targetDt=20120101").bind(to)
-        observable.bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { _, movie, cell in
-            if let cellToUse = cell as? MovieTableCell {
-                cellToUse.movieName.text = MovieInfo().ran
+        self.navigationController?.navigationBar.isHidden = true
+        viewModel.loadView(url: "").subscribe(onNext: { emitter in
+            tableView.rx.items(cellIdentifier: "MyCell", cellType: MovieTableCell.self) { index, movie, cell in
+                if let cellToUse = cell { cellToUse.  }
+                cellToUse.text
             }
-        }.disposed(by: disposeBag)
+        })
     }
 }
 
