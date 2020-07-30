0ad15a666bab893ea3c82a41cfbb04aabfe9715a
이현욱 <hjhsha827@gmail.com>
2020-07-30 23:53:01 +0900

    [Fix] MovieReloadViewModel 코드 수정

--- MovieService/MovieService/ViewModel/MovieReloadViewModel.swift
+++ MovieService/MovieService/ViewModel/MovieReloadViewModel.swift
@@ -1,27 +1,32 @@
-import Foundation
+import UIKit
 import RxSwift
 import RxCocoa
 import Alamofire
 
 class MovieReloadViewModel {
-    // 랭킹있고 이름있고 등등
-    //movie/searchMovieList.json?keay=c16f104dccf7480daa4204c8f921d8b6
     
     private let disposeBag = DisposeBag()
     private let key = "c16f104dccf7480daa4204c8f921d8b6"
     
     func loadView(url: String) -> Observable<Result> {
+        
         return Observable<Result>.create { observer in
-            let url = URL(string: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/" + url)!
-            AF.request(url, method: .get).responseData { response in
+            let url = URL(string: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101")!
+              //  http://www.kobis.or.kr/kobisopenapi/webservice/rest/" + newUrl)!
+            AF.request(url, method: .get)
+                .responseData { response in
+                print(response.result)
                 switch response.result {
                 case .success(let json):
+                    print(json)
                     do {
                         let model = try? JSONDecoder().decode(Result.self, from: json)
-                        observer.onNext(model!)
+                        observer.
+                        single(.success(model!))
+                        print(model)
                     } catch let error {
                         print(error.localizedDescription)
-                        observer.onError(error)
+                        single(.error(let error))
                     }
                 case .failure(let error) :
                     print(error.localizedDescription)
