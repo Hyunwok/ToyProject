0eda0a2d49d0b117d5826234fa93ac0645357bb1
이현욱 <hjhsha827@gmail.com>
2020-08-01 23:31:56 +0900

    [Add] APIService 파일 추가

---
+++ MovieService/MovieService/APIService.swift
@@ -0,0 +1,58 @@
+//
+//  APIService.swift
+//  MovieService
+//
+//  Created by 이현욱 on 2020/07/31.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+import Alamofire
+
+protocol APIRequest {
+    var method:String { get }
+    var path:String { get }
+    var parameter:[String:Any] { get }
+}
+
+extension APIRequest {
+    func request(with baseURL: URL) -> URLRequest {
+        var request = URLRequest(url: baseURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        return request
+    }
+}
+
+class APIService {
+    func loadView<T: Codable>(url: String) -> Observable<T> {
+        // Please input base url
+        let baseURL:URL = URL(string: "http://www.kobis.or.kr/kobisopenapi/webservice")!
+        
+        return Observable<T>.create { observer in
+            let url = URL(string: "/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101")!
+              //  http://www.kobis.or.kr/kobisopenapi/webservice/rest/" + newUrl)!
+            AF.request(url, method: .get).responseData { response in
+                switch response.result {
+                case .success(let json):
+                    do {
+                        let model = try? JSONDecoder().decode(T.self, from: json)
+
+                        observer.onNext(model!)
+                        //self.places.accept(model!)
+                    } catch let error {
+                        observer.onError(error)
+                    }
+                case .failure(let error) :
+                    print(error.localizedDescription)
+                    observer.onError(error)
+                }
+                observer.onCompleted()
+            }
+            return Disposables.create {
+                AF.cancelAllRequests()
+            }
+        }
+    }
+}
