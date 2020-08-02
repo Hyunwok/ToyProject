69c3804a147d37eb171ebbaf07018793f8f657c7
이현욱 <hjhsha827@gmail.com>
2020-08-02 23:38:58 +0900

    [Fix] 코드 수정

--- MovieService/MovieService/View/MainPageVC.swift
+++ MovieService/MovieService/View/MainPageVC.swift
@@ -16,7 +16,9 @@ class MainPageVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
-        let result = self.viewModel.loadView(url: "").bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { index, _, cell in
+        print(self.viewModel.loadView(url: ""))
+        let result : Observable<Result> = self.viewModel.loadView(url: "").asObservable()
+        result.bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { index, _, cell in
             if let cellToUse = cell as? MovieTableCell {
                 emitter.boxOfficeResult.dailyBoxOfficeList[index].movieNm
             }
