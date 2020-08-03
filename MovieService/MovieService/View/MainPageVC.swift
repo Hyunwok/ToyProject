d912514a258397fd80d919d32c68365f3df74aeb
이현욱 <hjhsha827@gmail.com>
2020-08-03 23:46:12 +0900

    [Fix] 코드 수정

--- MovieService/MovieService/View/MainPageVC.swift
+++ MovieService/MovieService/View/MainPageVC.swift
@@ -9,7 +9,6 @@ class MainPageVC: UIViewController {
     let viewModel = MovieReloadViewModel()
     let disposeBag = DisposeBag()
     let apiService = APIService()
-    let test = BehaviorSubject<[String]>(value: ["1","2","3"])
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -17,12 +16,18 @@ class MainPageVC: UIViewController {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
         print(self.viewModel.loadView(url: ""))
-        let result : Observable<Result> = self.viewModel.loadView(url: "").asObservable()
-        result.bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { index, _, cell in
-            if let cellToUse = cell as? MovieTableCell {
-                emitter.boxOfficeResult.dailyBoxOfficeList[index].movieNm
-            }
+        viewModel.loadView(url: "").asObservable()
+            .bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { index, movie, cell in
+                if let cellToUse = cell as? MovieTableCell {
+                    cellToUse.textLabel.text = cell.boxOfficeResult.dailyBoxOfficeList[index].movieNm
+                }
         }
+        //        let result : Observable<Result> = self.viewModel.loadView(url: "").asObservable()
+        //        result.bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { index, _, cell in
+        //            if let cellToUse = cell as? MovieTableCell {
+        //                emitter.boxOfficeResult.dailyBoxOfficeList[index].movieNm
+        //            }
+        //        }
     }
 }
 
