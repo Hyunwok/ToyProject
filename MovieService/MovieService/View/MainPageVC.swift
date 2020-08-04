d3e85349d659481b99405c7356361f551ae7697a
이현욱 <hjhsha827@gmail.com>
2020-08-04 23:49:37 +0900

    [Fix] MainPageVC 코드 수정

--- MovieService/MovieService/View/MainPageVC.swift
+++ MovieService/MovieService/View/MainPageVC.swift
@@ -3,7 +3,7 @@ import RxSwift
 import RxCocoa
 import Alamofire
 
-class MainPageVC: UIViewController {
+class MainPageVC: UIViewController, UITableViewDelegate {
     
     private let key = "430156241533f1d058c603178cc3ca0e"
     let viewModel = MovieReloadViewModel()
@@ -14,20 +14,31 @@ class MainPageVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBar.isHidden = true
-        print(self.viewModel.loadView(url: ""))
-        viewModel.loadView(url: "").asObservable()
-            .bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { index, movie, cell in
-                if let cellToUse = cell as? MovieTableCell {
-                    cellToUse.textLabel.text = cell.boxOfficeResult.dailyBoxOfficeList[index].movieNm
-                }
-        }
-        //        let result : Observable<Result> = self.viewModel.loadView(url: "").asObservable()
-        //        result.bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { index, _, cell in
-        //            if let cellToUse = cell as? MovieTableCell {
-        //                emitter.boxOfficeResult.dailyBoxOfficeList[index].movieNm
-        //            }
-        //        }
+
+        self.tableView.register(UINib(nibName: "MoiveTableViewCell", bundle: nil), forCellReuseIdentifier: "MyCell")
+        //        tableView.rx.setDataSource(self).dispose
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
+//        let nibName = UINib(nibName: MoiveTableViewCell.xibName, bundle: nil)
+        
+//        viewModel.loadView(url: "").map { [$0] }
+//            .bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { index, movie, cell in
+//                if let cellToUse = cell as? MoiveTableViewCell {
+//                    let movieIndex = movie.boxOfficeResult.dailyBoxOfficeList[index]
+//                    cellToUse.movieName.text = movieIndex.movieNm
+//                    cellToUse.movieOpenDate.text = movieIndex.openDt
+//                    cellToUse.cumulativeAudience.text = movieIndex.audiAcc
+//                    cellToUse.rank.text = movieIndex.rank
+//                }
+//        }.disposed(by: disposeBag)
+        
+        viewModel.loadView(url: "").map { [$0] }
+            .bind(to: tableView.rx.items(cellIdentifier: "MyCell", cellType: MoiveTableViewCell.self)) { index, movie, cell in
+                    let movieIndex = movie.boxOfficeResult.dailyBoxOfficeList[index]
+                    cell.movieName.text = movieIndex.movieNm
+                    cell.movieOpenDate.text = movieIndex.openDt
+                    cell.cumulativeAudience.text = movieIndex.audiAcc
+                    cell.rank.text = movieIndex.rank
+        }.disposed(by: disposeBag)
     }
 }
 
