e21f8fd4f0224ce2b7a8dc3f63aa4f3ed6000763
이현욱 <hjhsha827@gmail.com>
2020-08-05 23:49:05 +0900

    [Fix] Extension 수정

--- MovieService/MovieService/Extension/Extension.swift
+++ MovieService/MovieService/Extension/Extension.swift
@@ -1,7 +1,17 @@
 import UIKit
 
+enum StorybardName: String {
+    case detail = "DetailVC"
+}
+
 extension UIViewController {
 
+    func controller<T>(modal:Bool = false, name: StorybardName) -> T? {
+        guard let vc:T = self.storyboard?.instantiateViewController(identifier: name.rawValue) as? T else { return nil }
+        
+        return vc
+    }
+    
     func presentAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "확인", style: .cancel, handler: nil))
