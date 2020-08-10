0f2e6bd23a08f68b6a7c9a30c14222b7cdc0c66f
이현욱 <hjhsha827@gmail.com>
2020-08-10 19:55:45 +0900

    [Fix] 코드 수정

--- MovieService/MovieService/ViewModel/MovieReloadViewModel.swift
+++ MovieService/MovieService/ViewModel/MovieReloadViewModel.swift
@@ -3,41 +3,6 @@ import RxSwift
 import RxCocoa
 import Alamofire
 
-class MovieReloadViewModel {
-    
-    private let disposeBag = DisposeBag()
-    private let key = "c16f104dccf7480daa4204c8f921d8b6"
-    
-    func loadView(url: String) -> Observable<[List]> {
-        
-        return Observable<[List]>.create { observer in
-            let url = URL(string: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101")!
-              //  http://www.kobis.or.kr/kobisopenapi/webservice/rest/" + newUrl)!
-            
-            let requset = AF.request(url, method: .get).responseData { response in
-                switch response.result {
-                case .success(let json):
-                    do {
-                        let model = try JSONDecoder().decode(Result.self, from: json)
-                        let resultList = model.boxOfficeResult
-                        let finalResult = resultList.dailyBoxOfficeList
-                        observer.onNext(finalResult)
-                    } catch let error {
-                        observer.onError(error)
-                    }
-                case .failure(let error) :
-                    print(error.localizedDescription)
-                    observer.onError(error)
-                }
-                observer.onCompleted()
-            }
-            return Disposables.create {
-                requset.cancel()
-            }
-        }
-    }
-}
-
 class LogInViewModel {
     let idObservable = PublishSubject<String>()
     let pwObservable = PublishSubject<String>()
