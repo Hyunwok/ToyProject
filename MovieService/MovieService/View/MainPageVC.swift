4c8121468686f522886f2e88519679487db58791
이현욱 <hjhsha827@gmail.com>
2020-08-11 23:55:29 +0900

    [Fix] 코드 수정

--- MovieService/MovieService/View/MainPageVC.swift
+++ MovieService/MovieService/View/MainPageVC.swift
@@ -6,28 +6,28 @@ import Alamofire
 class MainPageVC: UIViewController {
     
     private let key = "430156241533f1d058c603178cc3ca0e"
-    let viewModel = MovieReloadViewModel()
+   // let viewModel = APIService()
     let disposeBag = DisposeBag()
-    let apiService = APIService()
+    //let apiService = APIService()
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
-        
-        viewModel.loadView(url: "")
-            .bind(to: tableView.rx.items(cellIdentifier: MovieCell.xibName, cellType: MovieCell.self)) { index, movie, cell in
-                cell.movie = movie
-        }.disposed(by: disposeBag)
-        
-        Observable.zip(self.tableView.rx.itemSelected, self.tableView.rx.modelSelected(List.self))
-            .bind { [unowned self] indexPath, model in
-                guard let vc:DetailVC = self.controller(name: .detail) else { return }
-                vc.item = model
-                self.navigationController?.pushViewController(vc, animated: true)
-        }.disposed(by: self.disposeBag)
-        
+//        
+//        self.viewModel.loadView(api: )
+//            .bind(to: tableView.rx.items(cellIdentifier: MovieCell.xibName, cellType: MovieCell.self)) { index, movie, cell in
+//                cell.movie = movie
+//        }.disposed(by: disposeBag)
+//        
+//        Observable.zip(self.tableView.rx.itemSelected, self.tableView.rx.modelSelected(List.self))
+//            .bind { [unowned self] indexPath, model in
+//                guard let vc:DetailVC = self.controller(storyBoardName: "MainPage", name: .detail) else { return }
+//                vc.item = model
+//                self.navigationController?.pushViewController(vc, animated: true)
+//        }.disposed(by: self.disposeBag)
+//        
         tableView.rowHeight = 120
     }
 }
