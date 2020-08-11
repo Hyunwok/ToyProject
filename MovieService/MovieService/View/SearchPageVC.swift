eef9a7a7c9e58b79d41fa739c8469c153df29d3c
이현욱 <hjhsha827@gmail.com>
2020-08-11 23:55:16 +0900

    [Fix] 코드 수정

--- MovieService/MovieService/View/SearchPageVC.swift
+++ MovieService/MovieService/View/SearchPageVC.swift
@@ -3,7 +3,9 @@ import RxSwift
 
 class SearchPageVC: UIViewController {
     
-    let viewModel = MovieReloadViewModel()
+    private let baseUrl = ""
+    
+    let apiService = ApiService()
     let disposeBag = DisposeBag()
     
     @IBOutlet weak var searchBar: UISearchBar!
@@ -11,26 +13,17 @@ class SearchPageVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.searchBar.rx.text
-            .flatMap { _ in
-                return self.viewModel.loadView(url: "")
-                    .bind(to: tableView.rx.items(cellIdentifier: "SearchCell")) { index, movie, cell in
-                        cell.textLabel?.text = movie.movieNm
-                }
+        self.searchBar.rx.text.orEmpty
+            .asObservable()
+            .map { $0.lowercased() }
+            .map { SearchMovieRequest(query: $0) }
+            .flatMap { request -> Observable<[SearchMovieInfo]> in
+//                return self.apiService.get(apiRequest: request, headers: ["Content-Type" : "application/json","":""], url: url.naver)
+                return self.apiService.get(apiRequest: request, url: .naver)
         }
-//        self.searchBar.rx.text.asObservable()
-//            .map { ($0 ?? "").lowercased() }
-//            .map { UniversityRequest(name: $0) }
-//            .flatMap { request -> Observable<[Result]> in
-//                return self.apiClient.send(apiRequest: request) }.bind(to: tableView.rx.items(cellIdentifier: "MyCell")) { index, model, cell in
-//                    cell.textLabel?.text = model.name
-//        }.disposed(by: disposeBag)
-//
-//        tableView.rx.items(cellIdentifier: "MyCell") { index, movie, cell in
-//            if let cellToUse = cell {
-//                cellToUse.textLabel.text = mo
-//            }
-//        }
-        
+        .bind(to: tableView.rx.items(cellIdentifier: "searchCell")) { index, model, cell in
+            cell.textLabel?.text = model.title
+        }
+        .disposed(by: disposeBag)
     }
 }
