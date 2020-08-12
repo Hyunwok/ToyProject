d152328f96008846ab5d81af5db2029852af01d1
이현욱 <hjhsha827@gmail.com>
2020-08-12 23:40:56 +0900

    [Fix] 코드 수정

--- MovieService/MovieService/View/MainPageVC.swift
+++ MovieService/MovieService/View/MainPageVC.swift
@@ -3,31 +3,55 @@ import RxSwift
 import RxCocoa
 import Alamofire
 
+//MARK: MainPageVC
 class MainPageVC: UIViewController {
     
-    private let key = "430156241533f1d058c603178cc3ca0e"
-   // let viewModel = APIService()
     let disposeBag = DisposeBag()
-    //let apiService = APIService()
+    let apiService = APIService()
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
-//        
-//        self.viewModel.loadView(api: )
-//            .bind(to: tableView.rx.items(cellIdentifier: MovieCell.xibName, cellType: MovieCell.self)) { index, movie, cell in
+        let dateString = getDateString(format: "yyyyMMdd", date: Date())
+        
+//        self.apiService.loadMovie(apiRequest: ChartMovieRequest(date: ""))
+//            .map{ [$0]}
+//            .bind(to: tableView.rx.items(cellIdentifier: MovieCell.xibName, cellType: MovieCell.self) { index, movie, cell in
 //                cell.movie = movie
-//        }.disposed(by: disposeBag)
-//        
-//        Observable.zip(self.tableView.rx.itemSelected, self.tableView.rx.modelSelected(List.self))
-//            .bind { [unowned self] indexPath, model in
-//                guard let vc:DetailVC = self.controller(storyBoardName: "MainPage", name: .detail) else { return }
-//                vc.item = model
-//                self.navigationController?.pushViewController(vc, animated: true)
-//        }.disposed(by: self.disposeBag)
-//        
+//            }
+//            .disposed(by: disposeBag)
+//        )
+        
+//        self.apiService
+//            .loadMovie(apiRequest: ChartMovieRequest(date: dateString)).flatMap { request -> Observable<ChartMovieRequest> in
+//                return self.apiService
+//                    .bind(to: tableView.rx.items(cellIdentifier: MovieCell.xibName, cellType: MovieCell.self)) { index, movie, cell in
+//                        cell.movie = movie
+//                }.disposed(by: disposeBag)
+//                //.flatMap { request -> Observable<[SearchMovieInfo]> in
+//                //                return self.apiService.get(apiRequest: request, headers: ["Content-Type" : "application/json","":""], url: url.naver)
+//                return self.apiService.loadMovie(apiRequest: request, url: .naver)
+//        }
+        
+        Observable.zip(self.tableView.rx.itemSelected, self.tableView.rx.modelSelected(List.self))
+            .bind { [unowned self] indexPath, model in
+                guard let vc:DetailVC = self.controller(storyBoardName: "MainPage", name: .detail) else { return }
+                vc.item = model
+                self.navigationController?.pushViewController(vc, animated: true)
+        }.disposed(by: self.disposeBag)
+        
         tableView.rowHeight = 120
+    }
+}
+
+//MARK: ChartMovieRequest
+class ChartMovieRequest: APIRequest {
+    var path = "key=430156241533f1d058c603178cc3ca0e&"
+    var parameters = [String : String]()
+    
+    init(date: String) {
+        parameters["targetDt"] = date
     }
 }
