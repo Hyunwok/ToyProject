bff944c2b7a39ba8d4c46c7d701d85bc43fcd16a
이현욱 <hjhsha827@gmail.com>
2020-10-23 23:24:16 +0900

    [Modify] Modify Code

--- MemoAlarm/MemoAlarm/AlarmVC.swift
+++ MemoAlarm/MemoAlarm/AlarmVC.swift
@@ -8,54 +8,35 @@ import RxSwift
 
 class AlarmVC: UIViewController, UNUserNotificationCenterDelegate {
     
+    let obs: Observable<[Date]>?
+    
     var calendarDataSource: [String:String] = ["2020-08-07":"잉기모띠"]
-    let commentString = BehaviorRelay<[String]>(value: [])
+    let commentString: BehaviorRelay<[String]> = .init(value: [""])
     let userNoti = UNNotiManager()
     var listDate: String!
     var listText = [String]()
     var list = [String]()
-    lazy var dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        return formatter
-    }()
+    
     
     @IBOutlet weak var datePickerXib: DatePickerXib!
     @IBOutlet weak var alarmPlusBtn: UIButton!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var calendarView: FSCalendar!
-    @IBOutlet weak var subjectXib: SubjectXib!
-    @IBOutlet weak var showWhenTouchPlus: UIButton!
+//    @IBOutlet weak var showWhenTouchPlus: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setDelegate()
         self.xibAndBtnIsHidden(value: true)
-        self.aboutCalendar()
+        self.calendarSetting()
         self.reloadUserDefault()
-        self.subjectXib.isHidden = true
         tableView.register(AlarmCell.self, forCellReuseIdentifier: AlarmCell.identifier)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisa(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        commentString.bind(to: tableView.rx.items(cellIdentifier: "")) { index, comment, cell in
-            if let newCell = cell as? AlarmCell {
-                newCell.alarmText.text = comment
-            }
-        }
-    }
-    
-    @IBAction func changeXib(_ sender: UIButton) {
-        if sender.isSelected {
-            hideXib(value: true)
-        } else {
-            hideXib(value: false)
-        }
-        sender.isSelected = !sender.isSelected
     }
     
     @IBAction func presentXib(_ sender: UIButton) {
-        self.xibAndBtnIsHidden(value: false)
+        self.xibAndBtnIsHidden(value: sender.isSelected)
         calendarView.reloadData()
     }
     
@@ -96,60 +77,37 @@ extension AlarmVC: DatePickerXibDelegate  {
             xibAndBtnIsHidden(value: boolean)
             return
         } else {
-            self.showToast(text: "텍스트가 비었습니다")
+            showToast(text: "텍스트가 비었습니다")
         }
-    }
-    
-    func hideXib(value: Bool) {
-        calendarView.isHidden = !value
-        subjectXib.isHidden = value
     }
     
     func setDelegate() {
         UNUserNotificationCenter.current().delegate = self
-        tableView.delegate = self
-        tableView.dataSource = self
+//        tableView.delegate = self
+//        tableView.dataSource = self
         calendarView.delegate = self
         datePickerXib.delegate = self
         calendarView.dataSource = self
     }
     
     func xibAndBtnIsHidden(value: Bool) {
-        showWhenTouchPlus.isHidden = value
+//        showWhenTouchPlus.isHidden = value
         datePickerXib.isHidden = value
     }
     
-    func showToast(text: String) {
-        let toastLabel = UILabel(frame: CGRect(x: view.frame.size.width/2 - 150, y: view.frame.size.height - 500, width: 300,  height : 35))
-        toastLabel.backgroundColor = UIColor.black
-        toastLabel.textColor = UIColor.white
-        toastLabel.textAlignment = .center;
-        view.addSubview(toastLabel)
-        toastLabel.text = text
-        toastLabel.font = UIFont.boldSystemFont(ofSize: 18)
-        toastLabel.alpha = 0.8
-        toastLabel.layer.cornerRadius = 10;
-        toastLabel.clipsToBounds  =  true
-        
-        UIView.animate(withDuration: 1.0, animations: {
-            toastLabel.alpha = 0.0
-        }, completion: {
-            (isBool) -> Void in
-            self.dismiss(animated: true, completion: nil)
-        })
-    }
+    
 }
 
 extension AlarmVC: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
     func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
-        let dateString = self.dateFormatter.string(from: date)
+        let dateString = dateFormatter.string(from: date)
         if self.list.contains(dateString) {
             return 1
         }
         return 0
     }
     
-    func aboutCalendar() {
+    func calendarSetting() {
         let ca = calendarView.appearance
         ca.headerMinimumDissolvedAlpha = 0.0;
         ca.eventOffset = CGPoint(x: 15, y: -35)
@@ -162,26 +120,15 @@ extension AlarmVC: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateA
     func calendar(_ calendar: FSCalendar, didselect date: Date, at monthPosition: FSCalendarMonthPosition) {
         let date = dateFormatter.string(from: date)
         if let comment = calendarDataSource[date] {
-            commentString.accept(comment)
+//            commentString.accept(comment)
         }
     }
 }
 
-extension AlarmVC: UITextFieldDelegate {
-    @objc func keyboardWillShow(_ sender: Notification) {
-        self.view.frame.origin.y = -150
-    }
-    
-    @objc func keyboardWillDisa(_ sender: Notification) {
-        self.view.frame.origin.y = 0
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
-        self.view.endEditing(true)
-    }
-}
+
+
+let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd"
+    return formatter
+}()
--- MemoAlarm/MemoAlarm/Base/Xib/DatePickerXib.swift
+++ MemoAlarm/MemoAlarm/Base/Xib/DatePickerXib.swift
@@ -1,18 +1,13 @@
 import UIKit
+
 import RxSwift
 
 final class DatePickerXib: UIView {
     
-    var alarmDate: String!
     var delegate: DatePickerXibDelegate?
     private let noti = UNNotiManager()
     private let xibName = "DatePickerXib"
-    lazy var dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "ko_KR")
-        formatter.dateFormat = "yyyy-MM-dd"
-        return formatter
-    }()
+    
     
     @IBOutlet weak var alarmTextField: UITextField!
     @IBOutlet weak var lbl: UILabel!
@@ -30,27 +25,22 @@ final class DatePickerXib: UIView {
     }
     
     private func commonInit(){
-        let view = Bundle.main.loadNibNamed(xibName, owner: self, options: nil)?.first as! UIView
+        let view = Bundle.main.loadNibNamed(self.xibName, owner: self, options: nil)?.first as! UIView
         view.frame = self.bounds
         self.addSubview(view)
     }
     
     @IBAction func getOk(_ sender: UIButton) {
-        if alarmTextField.text!.isEmpty {
-            delegate?.listAppend(value: List.init(dateList:"", dateListText: ""), boolean: false)
-            return
-        } else {
-            delegate?.listAppend(value: List.init(dateList:alarmDate, dateListText: alarmTextField.text!), boolean: true)
+        if !(alarmTextField.text?.isEmpty ?? false) {
+            delegate?.listAppend(value: List.init(dateList: alarmTextField.text ?? "", dateListText: alarmTextField.text!), boolean: true)
         }
     }
     
     @objc func changeDatePicker(_ sender: UIDatePicker) {
-        var list = [Date]()
-        self.alarmDate = self.dateFormatter.string(from: sender.date)
-        let koreaBeforeDate = self.dateFormatter.date(from: alarmDate)
-        noti.showEduNotification(date: koreaBeforeDate! + 3600 * 9 - 86400)
-        list.append(koreaBeforeDate!)
-        UserDefaults.standard.set(list, forKey: "dayList")
+        noti.showEduNotification(date: sender.date + 3600 * 9 - 86400)
+        obs?.subscribe(onNext: {_ in
+            [sender.date]
+        }).dispose()
     }
 }
 
