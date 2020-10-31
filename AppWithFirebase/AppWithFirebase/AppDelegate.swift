69dcc1374d4de591cef6674668381e34b9130eb0
이현욱 <hjhsha827@gmail.com>
2020-11-01 03:44:49 +0900

    [Start] Project Start

---
+++ AppWithFirebase/AppWithFirebase/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  AppWithFirebase
+//
+//  Created by 이현욱 on 2020/10/27.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+  
+
+}
+
---
+++ AppWithFirebase/AppWithFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppWithFirebase
+//
+//  Created by 이현욱 on 2020/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
