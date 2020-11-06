1b54ebd4d94bdd4041f591c33f17b33e672ec96b
이현욱 <hjhsha827@gmail.com>
2020-11-06 23:51:04 +0900

    [Modify] AlarmVC

--- MemoAlarm/MemoAlarm/AlarmVC.swift
+++ MemoAlarm/MemoAlarm/AlarmVC.swift
@@ -9,32 +9,22 @@ import RxSwift
 
 class AlarmVC: UIViewController, UNUserNotificationCenterDelegate {
     let userNoti = UNNotiManager()
-    var list = [String]()
+    private var bool = false
+    private let disposeBag = DisposeBag()
     
     @IBOutlet weak var datePickerXib: DatePickerXib!
     @IBOutlet weak var alarmPlusBtn: UIButton!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var calendarView: FSCalendar!
-    @IBOutlet weak var hiddenBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setDelegate()
+        UNUserNotificationCenter.current().delegate = self
         self.calendarSetting()
+        bindAction()
         tableView.register(AlarmCell.self, forCellReuseIdentifier: AlarmCell.identifier)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisa(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @IBAction func presentXib(_ sender: UIButton) {
-        self.xibAndBtnIsHidden(value: sender.isSelected)
-        calendarView.reloadData()
-    }
-    
-    @IBAction func disMissXib(_ sender: UIButton) {
-        self.xibAndBtnIsHidden(value: sender.isSelected)
-        calendarView.reloadData()
-//        reloadUserDefault()
     }
 }
 
@@ -42,34 +32,36 @@ class AlarmVC: UIViewController, UNUserNotificationCenterDelegate {
 //MARK: extension
 
 extension AlarmVC {
-    func setDelegate() {
-        UNUserNotificationCenter.current().delegate = self
-        tableView.delegate = self
-        tableView.dataSource = self
+    func bindAction() {
+        alarmPlusBtn.rx.tap
+            .subscribe({_ in
+                self.datePickerXib.isHidden = false
+            })
+            .disposed(by: disposeBag)
+        
+        datePickerXib.okBtn.rx.tap
+            .map { self.tableView.reloadData() }
+            .map { self.datePickerXib.alarmTextField.text?.isEmpty ?? false }
+            .subscribe(onNext: { b in
+                if !b {
+                    self.datePickerXib.isHidden = true
+                }
+            }).disposed(by: disposeBag)
+    }
+    
+    func bindUI() {
+        datePickerXib.alarmList?
+            .asObservable()
+            .map { [$0] }
+            .bind(to: tableView.rx.items(cellIdentifier: AlarmCell.identifier, cellType: AlarmCell.self)) { _, list, cell in
+                cell.alarmText.text = list.dateListText
+                self.tableView.reloadData()
+            }.disposed(by: disposeBag)
+        }
+    
+    func calendarSetting() {
         calendarView.delegate = self
         calendarView.dataSource = self
-    }
-    
-    func xibAndBtnIsHidden(value: Bool) {
-        hiddenBtn.isHidden = value
-        datePickerXib.isHidden = value
-    }
-}
-
-extension AlarmVC: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-}
-
-extension AlarmVC: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
-    
-    // 캘린더 세팅
-    func calendarSetting() {
         calendarView.reloadData()
         let ca = calendarView.appearance
         ca.headerMinimumDissolvedAlpha = 0.0;
@@ -79,25 +71,27 @@ extension AlarmVC: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateA
         ca.headerTitleColor = .black
         ca.weekdayTextColor =  .black
     }
-    
-    // 이벤트 갯수 반환 함수
-    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
-//        let dateString = dateFormatter.string(from: date)
-//        list.append(dateFormatter.string(from: DatePickerXib.alarmList![0].date))
-//        if list.contains(dateString) {
-//            return 1
-//        }
-        return 0
-    }
+}
 
+extension AlarmVC: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+//    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+//        self.datePickerXib.alarmList?.subscribe(onNext: { lists in
+//            for i in 0...lists.count {
+//                if lists[i].date == date {
+//                }
+//            }
+//        }).disposed(by: disposeBag)
+//        return 0
+//    }
+    
     /// 선택한 날짜의 이벤트 보여주기
-    func calendar(_ calendar: FSCalendar, didselect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        let dateString = dateFormatter.string(from: date)
-        list.append(dateFormatter.string(from: DatePickerXib.alarmList![0].date))
-        if list.contains(dateString) {
-//            테이블뷰와 합체해야댐
-        }
-    }
+    //    func calendar(_ calendar: FSCalendar, didselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+    //        let dateString = dateFormatter.string(from: date)
+    //        list.append(dateFormatter.string(from: DatePickerXib.alarmList![0].date))
+    //        if list.contains(dateString) {
+    ////            테이블뷰와 합체해야댐
+    //        }
+    //    }
 }
 
 
@@ -107,3 +101,5 @@ let dateFormatter: DateFormatter = {
     formatter.dateFormat = "yyyy-MM-dd"
     return formatter
 }()
+
+
