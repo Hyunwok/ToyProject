22ab27da426ebd672ad3acb72d63ce310d74411d
이현욱 <hjhsha827@gmail.com>
2020-11-11 16:59:17 +0900

    Project Start

---
+++ HyunGG/HyunGG/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  HyunGG
+//
+//  Created by 이현욱 on 2020/11/11.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
